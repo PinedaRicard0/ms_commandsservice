c00209190f34d422426e477d722304e5406994e5
Ricardo Pineda Rubio <pinedaricardo07@gmail.com>
2022-07-14 22:24:13 -0500

    Adding controllers and actions

--- Controllers/PlatformsController.cs
+++ Controllers/PlatformsController.cs
@@ -1,5 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using CommandsService.Data;
+using CommandsService.Dtos;
+using Microsoft.AspNetCore.Mvc;
 using System;
+using System.Collections.Generic;
 
 namespace CommandsService.Controllers
 {
@@ -7,9 +11,20 @@ namespace CommandsService.Controllers
     [ApiController]
     public class PlatformsController : ControllerBase
     {
-        public PlatformsController()
+        private readonly ICommandRepo _repository;
+        private readonly IMapper _mapper;
+        public PlatformsController(ICommandRepo repository, IMapper mapper)
         {
+            _repository = repository;
+            _mapper = mapper;
+        }
 
+        [HttpGet]
+        public ActionResult<IEnumerable<PlatformReadDto>> GetPlatforms()
+        {
+            Console.WriteLine("-- Getting platforms from commands service");
+            var platformItems = _repository.GetAllPlatforms();
+            return Ok(_mapper.Map<IEnumerable<PlatformReadDto>>(platformItems));
         }
 
         [HttpPost]
